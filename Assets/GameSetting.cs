b4cb37a0ac115c171570be396641b09de7e4f695
Ferdian Priambodho <ferdianpriambodho@student.uns.ac.id>
2021-11-10 11:41:59 +0700

    Camera follov & world space canvas issue

--- Assets/GameSetting.cs
+++ Assets/GameSetting.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Cinemachine;
 
 public class GameSetting : MonoBehaviour
 {
     public List<GameObject> Characters;
     public static GameObject Player;
+    public CinemachineVirtualCamera cM_vcam;
     private void Start()
+    {
+        // Start action on call
+    }
+
+    public void instantiateCharacter()
     {
         Debug.Log("chara int " + PlayerPrefs.GetInt("chara"));
         if(SceneManager.GetActiveScene().buildIndex != 0)
         {
             Player = Instantiate(Characters[PlayerPrefs.GetInt("chara")]);
             Debug.Log(Player.name);
+            cM_vcam.m_Follow = Player.transform;
         }
     }
 }
